669206b79d5347c91e227b6ec3a6c7133ea4f41f
thegoodhacker96 <gloriamariasulbaran0@gmail.com>
2025-03-26 12:03:28 -0400

    Update Edad.py

--- Edad.py
+++ Edad.py
@@ -1,16 +1,22 @@
 #python 3.12
+#Importar sys
 
-#Entrada de datos
+import sys
 
-edad = int(input("¿Cuál es tu edad?: "))
+#Entrada de datos y bucle
+while True
+    print ("Escribe exit para romper el bucle y salir")
+    edad = int(input("¿Cuál es tu edad?: "))
 
 #Comprobando la respuesta del usuario
 
-if edad >= int(18):
+    if edad >= int(18):
     print ("Eres mayor de edad felicidades")
-else:
+    else:
     print ("Eres menor de edad prepárate y aprende para cuando seas mayor de edad")
-
+    if edad >= "exit":
+        break
+        sys.exit ()
 #Mensaje de despedida
 
 print ("")
