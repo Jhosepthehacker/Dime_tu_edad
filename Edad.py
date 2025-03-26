a55da22bb326f0171eeff68cd1f910407143a04c
Jhosepthehacker <josemanuelramirezrengifo9@gmail.com>
2025-03-26 11:07:14 -0400

    Create Edad.py

---
+++ Edad.py
@@ -0,0 +1,18 @@
+#python 3.12
+
+#Entrada de datos
+
+edad = int(input("¿Cuál es tu edad?: "))
+
+#Comprobando la respuesta del usuario
+
+if edad >= int(18):
+    print ("Eres mayor de edad felicidades")
+else:
+    print ("Eres menor de edad prepárate y aprende para cuando seas mayor de edad")
+
+#Mensaje de despedida
+
+print ("")
+
+print ("Espero que te haya gustado el programa")
